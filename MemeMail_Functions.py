059ccfd82ff5c83f6ffa31e1408720d0fdb65ee0
HaloSam296 <halosam296@gmail.com>
2019-05-02 08:49:42 -0500

    Updating and Commenting Code
    Added more and better comments
    Changed random out for quantumrandom
    Added a timer

--- MemeMail_Functions.py
+++ MemeMail_Functions.py
@@ -1,15 +1,3 @@
-def get_meme():
-    from NamesAndImagesLists import image_list
-    ran_meme = ran.randint(1,9) 
-    meme = image_list[ran_meme]
-    print(meme, '\n')
-
-def get_email():
-    from NamesAndImagesLists import email_list
-    ran_email = ran.randint(1, 6) 
-    email = email_list[ran_email]
-
-
 
 def download_meme():
     #this is for downloading a meme online and assigning it a name
@@ -25,70 +13,72 @@ def download_meme():
         print(":( \n")
 
 
-
 def MakeAndSendMemeMail():
-    import smtplib
-    import urllib.request
-    from email.mime.text import MIMEText
+    import quantumrandom as Qran                #quantumrandom is like random, but it uses a newer method to get better random 
+    import smtplib                              #number. It's called quantumrandom because it uses newer quantum computer
+    from email.mime.text import MIMEText        #technology.
     from email.mime.multipart import MIMEMultipart
     from email.mime.base import MIMEBase
     from email import encoders
     import os.path
-    import random as ran
+    import time as T
     from NamesAndImagesLists import image_list
     from NamesAndImagesLists import email_list
+    
+    start_time = T.time() #The timer starts here
 
-
-
-    #the next part of the code picks the meme from the selection
-    ran_meme = ran.randint(1,9)
+    #the program picks the meme with this
+    ran_meme = round(Qran.randint(1,9))
     meme = image_list[ran_meme]
     print(meme, '\n')
 
-    #this part picks the email for the meme to be sent to   
-    ran_email = ran.randint(1, 6)  
+
+    #this program picks the email to send to
+    ran_email = round(Qran.randint(1, 6))      
     email = email_list[ran_email]
 
+    
+    #for the two parts that randomly get the meme and email, be sure to set the number after Qran.randint. The second number
+    #should be the amount you have. I.e., if I have 3 memes and 2 emails it should look like this:
+    #    ran_meme = round(Qran.randint(1,3))
+    #    ran_email = round(Qran.randint(1, 2))   
 
     print('Starting\n')
 
-    sender = 'example@email.sam'
+    #This is for setting up the email in the code. Look at the readme for the guide on what settings you have to change
+    #in the email
+    sender = 'example@gmail.com'
     password = 'password'
     send_to_email = email 
 
-
-
-    subject = "You've got mememail"
+    #This is where you set the subject and text of the email
+    subject = "You've got MemeMail"
     message = ""
-    #file_location = [specific meme]
 
+
+    #This is the program taking the parameters we've set previously in the script and formatting them to be an email
     msg = MIMEMultipart()
     msg['From'] = sender
     msg['To'] = send_to_email
     msg['Subject'] = subject
+    
+    #This is converting the image into something that can be sent via email
     msg.attach(MIMEText(message, 'plain'))
-
     attachment = open(meme, "rb")
-    
-    
-    #this part converts the image into base64 so it can be sent with the email
-    #This bit of code is an amalgamation of code from online, it is not entirely credited towards me.
     part = MIMEBase('application', 'octet-stream')
     part.set_payload((attachment).read())
     encoders.encode_base64(part)
     part.add_header('Content-Disposition', "attachment; filename= %s" % meme)
     msg.attach(part)
-
     
-
+    
     print('Email Fully Built\n')
     print('Email is being sent to ', send_to_email, ' ')
-
-
-
+    
+    
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
+        server = smtplib.SMTP('smtp.gmail.com', 587)        #This part is the program connecting to the server and 
+        server.starttls()                                   #attempting to send the email
         server.login(sender, password)
         text = msg.as_string()
         server.sendmail(sender, send_to_email, text)
@@ -96,6 +86,11 @@ def MakeAndSendMemeMail():
         print('Success...?')
     except:
         print('Failed to either connect to server or to send the email')
+        
+    end_time = T.time() #The end of the timer
+    
+    print("Ending Program\n", "Finished in", end_time - start_time, "seconds.\n")   #Final message stating completion
+                                                                                    #and how long it took
 
 
 
